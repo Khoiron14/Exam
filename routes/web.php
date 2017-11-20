f1415c3afdb9d52daac5f806f0515c74f0b52c40
rikaanrokhim <rikaanrokhim1@gmail.com>
2017-11-20 12:03:30 +0700

    akses member

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ use Illuminate\Http\Request;
+ Route::get('/', function () {
+     return view('welcome');
+ });
+// Route untuk user yang baru register
+ Route::group(['prefix' => 'home', 'middleware' => ['auth']], function(){
+     Route::get('/', function(){
+         $data['role'] = \App\UserRole::whereUserId(Auth::id())->get();
+         return view('home', $data);
+     });
+     Route::post('upgrade', function(Request $request){
+         if($request->ajax()){
+             $msg['success'] = 'false';
+             $user = \App\User::find($request->id);
+             if($user)
+                 $user->putRole($request->level);
+                 $msg['success'] = 'true';
+ 
+             return response()
+                 ->json($msg);
+         }
+     });
+ });
+
+// Route untuk user yang member
+ Route::group(['prefix' => 'member', 'middleware' => ['auth','role:member']], function(){
+     Route::get('/', function(){
+         return view('member');
+     });
+ });
 
 Auth::routes();
 
